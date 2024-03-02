42944675411a4395ae531a247fa1e4ca4d360c0f
Sailesh Bellamkonda <4201725+SaileshBellamkonda@users.noreply.github.com>
2024-03-03 01:44:52 +0530

    Updated the tests

--- tests/8.0/simple/program.cs
+++ tests/8.0/simple/program.cs
@@ -1,7 +1,22 @@
-try {
-    Console.WriteLine("Hello, World!");
-} catch (Exception e) {
-    Console.WriteLine(e.Message);
-} finally {
-    Console.WriteLine("Goodbye, World!");
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+try
+{
+    string? framework = Assembly
+        .GetEntryAssembly()?
+        .GetCustomAttribute<TargetFrameworkAttribute>()?
+        .FrameworkName;
+
+    Console.WriteLine($"OS Description\t\t\t:\t{RuntimeInformation.OSDescription}");
+    Console.WriteLine($"OS Architecture\t\t\t:\t{RuntimeInformation.OSArchitecture}");
+    Console.WriteLine($"Process Architecture\t\t:\t{RuntimeInformation.ProcessArchitecture}");
+    Console.WriteLine($".NET Framework Name\t\t:\t{framework}");
+    Console.WriteLine($".NET Version\t\t\t:\t{RuntimeInformation.FrameworkDescription}");
+    Console.WriteLine($".NET Runtime Identifier\t\t:\t{RuntimeInformation.RuntimeIdentifier}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine($"Error: {ex}");
 }
